e4ec5d58bb99160415127724e2d8e4c8e0f2ccca
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-10-02 08:44:41 -0400

    10/2/18 update

--- src/JacksonJacobs/Jjacobslib.java
+++ src/JacksonJacobs/Jjacobslib.java
@@ -1,9 +1,21 @@
 package JacksonJacobs;
 
 public class Jjacobslib {
-    public static String reverse(String input)
+    public static String isPalindrome(String input)
     {
         String output = "";
-        int n = input.length() - 1
+        int n = 0;
+        for(n = input.length() - 1; n>0; n--)
+        {
+            output = output + input.substring(n,n+1);
+        }
+        return output;
+    }
+
+    public static String cutOut(String mainStr, String subStr)
+    {
+        String output = "";
+        output = mainStr.substring(mainStr.indexOf(subStr), mainStr.indexOf(subStr) + subStr.length() - 1);
+        return output;
     }
 }
--- src/JacksonJacobs/Main.java
+++ src/JacksonJacobs/Main.java
@@ -3,6 +3,7 @@ package JacksonJacobs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.printIn("palindrome: " + Jjacobslib.isPalindrome("good"));
+        System.out.printIn()
     }
 }
