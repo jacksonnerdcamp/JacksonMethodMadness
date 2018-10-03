4dd8277af2c2708b22997d7df4e3b1c8bd26c08a
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-10-03 08:41:57 -0400

    Added cutout method

--- src/JacksonJacobs/Jjacobslib.java
+++ src/JacksonJacobs/Jjacobslib.java
@@ -1,21 +1,22 @@
 package JacksonJacobs;
 
 public class Jjacobslib {
-    public static String isPalindrome(String input)
+    public static boolean isPalindrome(String input)
     {
         String output = "";
         int n = 0;
-        for(n = input.length() - 1; n>0; n--)
+        for(n = input.length() - 1; n>=0; n--)
         {
             output = output + input.substring(n,n+1);
         }
-        return output;
+        return input.equals(output);
     }
 
     public static String cutOut(String mainStr, String subStr)
     {
-        String output = "";
-        output = mainStr.substring(mainStr.indexOf(subStr), mainStr.indexOf(subStr) + subStr.length() - 1);
+        String output = mainStr.substring(0,mainStr.indexOf(subStr));
+        int secondSubIndex = mainStr.indexOf(subStr) + subStr.length();
+        output = output + mainStr.substring(secondSubIndex);
         return output;
     }
 }
