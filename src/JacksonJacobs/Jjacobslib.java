ff8f75f1db0aeb2bc878b3575546676841ae1a08
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-10-04 18:44:59 -0400

    added primePrinter

--- src/JacksonJacobs/Jjacobslib.java
+++ src/JacksonJacobs/Jjacobslib.java
@@ -2,6 +2,8 @@ package JacksonJacobs;
 //this is the indicator
 public class Jjacobslib {
     public static boolean isPalindrome(String input)
+    /*To check if the input was a palindrome, I created a new string and added every letter from the input from last to first.
+    Then I could check the output against the input to determine if it was a palindrome or not.*/
     {
         String output = "";
         int n = 0;
@@ -13,6 +15,9 @@ public class Jjacobslib {
     }
 
     public static String cutOut(String mainStr, String subStr)
+    /*The cutOut method did not require any loops, but I had to account for inputs where the subStr happened to be in
+    the middle. Therefore, I created a substring of characters before the target word, and a substring of characters after.
+    Finally I could concatenate these substrings and the output was returned.*/
     {
         String output = mainStr.substring(0,mainStr.indexOf(subStr));
         int secondSubIndex = mainStr.indexOf(subStr) + subStr.length();
@@ -21,6 +26,8 @@ public class Jjacobslib {
     }
 
     public static int sumUpTo(int input)
+    /*The sumUpTo method only required me to establish an int output that became the sum of consecutive integers as they
+    were counted up over a for loop. The loop terminated when the iterator reached the input value.*/
     {
         int output = 0;
         for(int counter = 0; counter <= input; counter++)
@@ -31,19 +38,23 @@ public class Jjacobslib {
     }
 
     public static boolean isPrime(int input)
+    /*I created this method so that the primePrinter method would be more readable. All this does is it checks if the
+    input is a prime number by finding the remainder of it divided by every number leading up to it. If input mod num is
+    zero (meaning num is a factor of the input) the output is set to false; otherwise it defaults to true.*/
     {
-        int output = 0;
+        boolean output = true;
         for (int n = 2; n < input; n++) {
             if (input % n == 0) {
-                output++;
+                output = false;
                 break;
             }
         }
-        if (output == 0) return true;
-        else return false;
+        return output;
     }
 
     public static int primePrinter(int num)
+    /*Because the prime number checker is already made, all this code needs to do is loop through the integers below the
+    input, while a counter increases each time a prime number is detected.*/
     {
         int counter = 0;
         for(int n = 3; n <= num; n++)
@@ -54,6 +65,12 @@ public class Jjacobslib {
             }
         }
         return counter;
+    }
+
+    public static String dateStr(String input)
+    //Nothing to see here, just using substrings to rearrange the input and return the new string.
+    {
+        return input.substring(3,5) + " - " + input.substring(0,2) + " - " + input.substring(6);
     }
 
 }
--- src/JacksonJacobs/Main.java
+++ src/JacksonJacobs/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("cutOut:" + Jjacobslib.cutOut("jackson", "ck"));
         System.out.println("sumUpTo: " + Jjacobslib.sumUpTo(2156));
         System.out.println("primePrinter: " + Jjacobslib.primePrinter(12));
+        System.out.println("dateStr: " + Jjacobslib.dateStr("02/27/2001"));
     }
 }
